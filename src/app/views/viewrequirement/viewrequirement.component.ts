290e37b69562fe40505bbe5247ddba8f21660da1
fourthdm <manisha.fourthdm@gmail.com>
2026-03-26 17:58:38 +0530

    Update a Requirement_items in Requirement by Item_Id  in view requirement Component

--- src/app/views/viewrequirement/viewrequirement.component.ts
+++ src/app/views/viewrequirement/viewrequirement.component.ts
@@ -14,6 +14,9 @@ export class ViewrequirementComponent {
   AllRequirementData: any[] = [];
   RequirementitemUpdate: FormGroup;
 
+  RequirementDetails: any = {};
+  // AllRequirementData: any[] = [];
+
   constructor(private _RestService: RestService, private _activatedRoute: ActivatedRoute, private Sanitizer: DomSanitizer) {
     this.RequirementitemUpdate = new FormGroup({
       Item_Id: new FormControl(''),
@@ -28,17 +31,43 @@ export class ViewrequirementComponent {
     this.Requirements();
   }
 
+  // Requirements() {
+  //   this._activatedRoute.params.subscribe(params => {
+  //     const Req_id = params['Req_id'];
+
+  //     this._RestService.Requirementdetails(Req_id).subscribe((res: any) => {
+
+  //       // ✅ Correct way
+  //       this.AllRequirementData = res.data?.items || [];
+
+  //       console.log('Items:', this.AllRequirementData);
+  //     });
+  //   });
+  // }
+
   Requirements() {
     this._activatedRoute.params.subscribe(params => {
       const Req_id = params['Req_id'];
+
       this._RestService.Requirementdetails(Req_id).subscribe((res: any) => {
-        this.AllRequirementData = Array.isArray(res.data)
-          ? res.data
-          : [res.data];
-        console.log('Requirement Data:', this.AllRequirementData);
+
+        this.RequirementDetails = res.data;     // full data
+        this.AllRequirementData = res.data?.items || []; // only items
+    console.log('Items:', this.AllRequirementData);
       });
     });
   }
+  // Requirements() {
+  //   this._activatedRoute.params.subscribe(params => {
+  //     const Req_id = params['Req_id'];
+  //     this._RestService.Requirementdetails(Req_id).subscribe((res: any) => {
+  //       this.AllRequirementData = Array.isArray(res.data)
+  //         ? res.data
+  //         : [res.data];
+  //       console.log('Requirement Data:', this.AllRequirementData);
+  //     });
+  //   });
+  // }
 
   deleteItem(Item_Id: any) {
     if (confirm("Delete this product?")) {
@@ -49,47 +78,66 @@ export class ViewrequirementComponent {
     }
   }
 
-  SelectItem: any = null;
-editrequirementitems(Item_Id: any) {
-  const selectedItem = this.AllRequirementData.find(item => item.Item_Id === Item_Id);
+  SelectItem: any = undefined;
+  editrequirementitems(Item_Id: any) {
 
-  if (selectedItem) {
-    this.SelectItem = 1;
-    this.RequirementitemUpdate.patchValue(selectedItem);
+    const selectedItem = this.AllRequirementData.find(
+      item => item.Item_Id === Item_Id
+    );
+
+    if (selectedItem) {
+      this.SelectItem = 1;
+
+      this.RequirementitemUpdate.patchValue({
+        Item_Id: selectedItem.Item_Id,
+        Product_Name: selectedItem.Product_Name,
+        Material_Type: selectedItem.Material_Type,
+        Product_Quantity: selectedItem.Product_Quantity,
+        HSN_Code: selectedItem.HSN_Code
+      });
+    }
   }
-}
-
   // editrequirementitems(Item_Id: any) {
-  //   const selectClient = this.AllRequirementData.find(Admin => Admin.Item_Id === Item_Id)
-  //   if (selectClient) {
+  //   const selectedItem = this.AllRequirementData.find(item => item.Item_Id === Item_Id);
+
+  //   if (selectedItem) {
   //     this.SelectItem = 1;
-  //     this.RequirementitemUpdate.patchValue(selectClient);
-  //   } else {
-  //     console.log(`Requirement Items with Item  ${Item_Id} not found.`);
+  //     this.RequirementitemUpdate.patchValue(selectedItem);
   //   }
   // }
-UpdateRequirementitems() {
-  if (this.RequirementitemUpdate.valid) {
 
-    this._RestService.updateRequirementiems(this.RequirementitemUpdate.value)
-      .subscribe((res: any) => {
+  UpdateRequirementitems() {
+    if (this.RequirementitemUpdate.valid) {
 
-        alert(res.message);
+      this._RestService.updateRequirementiems(this.RequirementitemUpdate.value)
+        .subscribe((res: any) => {
 
-        // ✅ Refresh list after update
-        this.Requirements();
+          alert(res.message);
 
-        // ✅ Reset form
-        this.RequirementitemUpdate.reset();
+          // ✅ Refresh list after update
+          this.Requirements();
 
-        // ✅ Hide edit mode
-        this.SelectItem = null;
+          // ✅ Reset form
+          this.RequirementitemUpdate.reset();
 
-      }, (err: any) => {
-        console.log(err);
-      });
+          // ✅ Hide edit mode
+          this.SelectItem = null;
 
+        }, (err: any) => {
+          console.log(err);
+        });
+
+    }
   }
-}
 
 }
+
+// editrequirementitems(Item_Id: any) {
+//   const selectClient = this.AllRequirementData.find(Admin => Admin.Item_Id === Item_Id)
+//   if (selectClient) {
+//     this.SelectItem = 1;
+//     this.RequirementitemUpdate.patchValue(selectClient);
+//   } else {
+//     console.log(`Requirement Items with Item  ${Item_Id} not found.`);
+//   }
+// }
